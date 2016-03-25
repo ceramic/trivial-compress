ae19465bce8189364546353247b5b8d3e5f459d2
Fernando Borretti <eudoxiahp@gmail.com>
2016-03-25 15:23:33 -0300

    Add the main interface

--- src/trivial-compress.lisp
+++ src/trivial-compress.lisp
@@ -1,4 +1,21 @@
 (in-package :cl-user)
 (defpackage trivial-compress
-  (:use :cl))
+  (:use :cl)
+  (:import-from :alexandria
+                :if-let)
+  (:import-from :which
+                :which)
+  (:documentation "The main compress interface."))
 (in-package :trivial-compress)
+
+(defun tar (directory archive)
+  "Create a tar archive from the contents of a directory."
+  (if-let (bin (which "tar"))
+    (trivial-compress.native:tar bin directory archive)
+    (trivial-compress.cl:tar directory archive)))
+
+(defun zip (directory archive)
+  "Create a zip archive from the contents of a directory."
+  (if-let (bin (which "zip"))
+    (trivial-compress.native:zip bin directory archive)
+    (trivial-compress.cl:zip directory archive)))
